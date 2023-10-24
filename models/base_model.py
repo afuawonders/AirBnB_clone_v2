3fbf984f7ee6828174fb03774f6ddf9115795712
afuawonders <afuawonders@gmail.com>
2023-10-24 20:54:39 +0000

    AirBnB clone 2

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,128 @@
 #!/usr/bin/python3
+
 """This module defines a base class for all models in our hbnb clone"""
+
 import uuid
+
 from datetime import datetime
 
 
+import models
+
+from uuid import uuid4
+
+from datetime import datetime
+
+from models import storage
+
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import Column, DateTime, String
+
+
+Base = declarative_base()
+
+
 class BaseModel:
+
+    """Defines the BaseModel class.
+
+    Attributes:
+
+        id (sqlalchemy String): The BaseModel id.
+
+        created_at (sqlalchemy DateTime): The datetime at creation.
+
+        updated_at (sqlalchemy DateTime): The datetime of last update.
+
+    """
+
+    id = Column(String(60), primary_key=True, nullable=False)
+
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     """A base class for all hbnb models"""
-    def __init__(self, *args, **kwargs):
+
+    def __init__(self, **kwargs):
+
         """Instatntiates a new model"""
+
+        print(kwargs)
+
         if not kwargs:
-            from models import storage
+
+            # from models import storage
+
             self.id = str(uuid.uuid4())
+
             self.created_at = datetime.now()
+
             self.updated_at = datetime.now()
-            storage.new(self)
+
+            # storage.new(self)
+
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+
+            if '_class_' in kwargs:
+
+                del kwargs['_class_']
+
+            self._dict_.update(kwargs)
 
     def __str__(self):
+
         """Returns a string representation of the instance"""
+
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+
+        return '[{}] ({}) {}'.format(cls, self.id, self._dict_)
 
     def save(self):
+
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
+
+        # from models import storage
+
         self.updated_at = datetime.now()
+
+        storage.new(self)
+
         storage.save()
 
     def to_dict(self):
+
         """Convert instance into dict format"""
+
         dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
+
+        dictionary.update(self._dict_)
+
+        if '_sa_instance_state' in dictionary:
+
+            dictionary.pop('_sa_instance_state')
+
+        dictionary.update({'_class_':
+
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
+
         dictionary['created_at'] = self.created_at.isoformat()
+
         dictionary['updated_at'] = self.updated_at.isoformat()
+
         return dictionary
+
+    def delete(self):
+
+        """Delete the current instance from storage."""
+
+        storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    # Set the table name for this class
+
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    # Define the 'state_id' column with constraints and set it as a foreign key
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    # Establish a relationship with the State class
+    places = relationship("Place", backref="cities", cascade="delete")
