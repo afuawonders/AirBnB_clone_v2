1f67bc848728637f54a3f0cc088b88aee78d3a10
Niamask <niamaskay@gmail.com>
2023-10-25 12:19:15 -0700

    Revert "AirBnB clone 2"
    This reverts commit 3fbf984f7ee6828174fb03774f6ddf9115795712.

--- models/__init__.py
+++ models/__init__.py
@@ -1,29 +1,7 @@
 #!/usr/bin/python3
+"""This module instantiates an object of class FileStorage"""
+from models.engine.file_storage import FileStorage
 
-"""This module instantiates an object of class FileStorage. This also defines
-the storage engine for the application."""
-
-
-from os import getenv
-
-# Check the value of the HBNB_TYPE_STORAGE environment variable
-
-if getenv("HBNB_TYPE_STORAGE") == "db":
-
-    from models.engine.db_storage import DBStorage
-
-    storage = DBStorage()
-
-    storage.reload()
-
-else:
-
-    from models.engine.file_storage import FileStorage
-
-    storage = FileStorage()
-
-    storage.reload()
 
 storage = FileStorage()
-
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,35 +1,8 @@
 #!/usr/bin/python3
-
 """ State Module for HBNB project """
-
 from models.base_model import BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
 
 
 class State(BaseModel):
     """ State class """
-
-    # Define the table name
-    __tablename__ = "states"
-
-    # Define a column for the 'name' attribute
-    name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state",
-        cascade="all, delete-orphan")
-
-    @property
-    def cities(self):
-        """ Getter attribute for cities """
-        from models import storage
-        city_list = []
-        for city in storage.all("City").values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
-
-
-storage = FileStorage()
-storage.reload()
+    name = ""
