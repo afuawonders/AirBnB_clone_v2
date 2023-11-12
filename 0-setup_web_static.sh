be44dd2ceec80612ba9c42848f7cd70f6703c8c5
afuawonders <afuawonders@gmail.com>
2023-11-12 04:28:37 +0000

    Airbnb

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,24 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Setting up my web servers for the deployment of web_static.
 
-apt-get update
-apt-get install -y nginx
+sudo apt update -y
+sudo apt install -y nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Holberton School</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+sudo chown -R ubuntu:ubuntu /data/
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=AJN9uU35VVs;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+sudo sed -i '60 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,64 +1,45 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+"""A script (based on the file 1-pack_web_static.py) that
+distributes an archive to your web servers,
+using the function do_deploy
+"""
+
+from datetime import datetime
+from fabric.api import *
+import os
 
 env.hosts = ["54.208.215.43", "52.91.151.149"]
+env.user = "ubuntu"
 
 
 def do_pack():
-    """
-        generates a .tgz archine from contents of web_static
-    """
-    time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    file_name = "versions/web_static_{}.tgz".format(time)
-    try:
-        local("mkdir -p ./versions")
-        local("tar --create --verbose -z --file={} ./web_static"
-              .format(file_name))
-        return file_name
-    except Exception:
+    """generating a .tzg archive"""
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    arc_path = "version/web_static_{}.tzg".format(date)
+    create_archive = local("tar -cvzf {} web_static".format(arc_path))
+    if create_archive.succeeded:
+        return archive_path
+    else:
         return None
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
+    """distributing the archives"""
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        version_dir = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(version_dir))
+        run("tar -xzf {} -C {}/".format(archived_file, version_dir))
+        run("rm {}".format(archived_file))
+        run("mv {}/web_static/* {}".format(version_dir, version_dir))
+        run("rm -rf {}/web_static".format(version_dir))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(version_dir))
 
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+        print("New version deployed!")
+        return True
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
+    return False
