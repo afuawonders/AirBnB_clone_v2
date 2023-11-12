dd0c52673189cae8aba341107cb8a90e7693b5ed
afuawonders <afuawonders@gmail.com>
2023-11-12 01:02:42 +0000

    Airbnb

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,19 @@ from fabric.api import run
 
 env.hosts = ["54.208.215.43", "52.91.151.149"]
 
+def do_pack():
+    """
+        generates a .tgz archine from contents of web_static
+    """
+    time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    file_name = "versions/web_static_{}.tgz".format(time)
+    try:
+        local("mkdir -p ./versions")
+        local("tar --create --verbose -z --file={} ./web_static"
+              .format(file_name))
+        return file_name
+    except:
+        return None
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
